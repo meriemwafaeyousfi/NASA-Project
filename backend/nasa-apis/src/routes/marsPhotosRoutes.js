5b2a7097c6b414f13a4661b119b032c6e97e35a1
Yousfi Meriem Wafae <36759575+meriemwafaeyousfi@users.noreply.github.com>
2025-02-27 20:48:33 +0100

    Delete backend/nasa-apis directory

--- backend/nasa-apis/src/routes/marsPhotosRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { fetchMarsPhotos } = require("../controllers/marsPhotosController");
-
-const router = express.Router();
-router.get("/", fetchMarsPhotos);
-
-module.exports = router;
